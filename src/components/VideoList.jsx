54db2e71b65cfebc6e6f00e153032890b3ffd012
Stu Hanford <shanford3@gmail.com>
2020-08-31 17:30:53 -0700

    Work on VideoList, VideoPlayer, and VideoListEntry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,13 @@
-var VideoList = () => (
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5>
+      {props.videos.map(video =>
+        <VideoListEntry video={video} />
+      )}
+    </h5></div>
   </div>
 );
 
