feca2602b46650d07fe487337316c6ea96c3f3dc
Stu Hanford <shanford3@gmail.com>
2020-09-01 17:30:06 -0700

    Complete except for debounce

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,15 @@
 var Search = ({ submitSearch }) => (
   <div className="search-bar form-inline">
-    <input className="form-control" id="searchBar" type="text" onChange={(text) => submitSearch(text.target.value)} />
+    <input className="form-control" id="searchBar" type="text" onChange={_.debounce((event) => {
+      event.persist();
+      submitSearch(event.target.value);
+    }, 500)} />
     <button className="btn hidden-sm-down">
       <span className="glyphicon glyphicon-search"></span>
     </button>
   </div>
 );
-
+//{_.debounce(props.func.bind(props.parent), 500)}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
